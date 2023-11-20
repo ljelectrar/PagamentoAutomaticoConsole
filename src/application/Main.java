1ec6a58cff8621997292850ac8fc6c873e804f94
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-11-20 19:58:28 -0300

    Implementação geral de todos os pacotes

---
+++ src/application/Main.java
@@ -0,0 +1,46 @@
+package application;
+
+import entities.Contract;
+import entities.Installment;
+import services.ContractService;
+import services.PaypalService;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Enter with the contract data: ");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Date: (dd/MM/yyyy): ");
+        LocalDate data = LocalDate.parse(sc.next(), fmt);
+        System.out.print("Contract value R$: ");
+        double contractValue = sc.nextDouble();
+
+        Contract contract = new Contract(number, data, contractValue);
+
+        System.out.print("Enter with the number of installments: ");
+        int instalments = sc.nextInt();
+
+        ContractService contractService = new ContractService(new PaypalService());
+
+        contractService.processContract(contract, instalments);
+
+        System.out.println("--- PARCELAS ---");
+
+        for (Installment installment : contract.getInstallments()) {
+            System.out.println(installment + "\n");
+        }
+        sc.close();
+    }
+
+}
